bd98351ffefb060f4ae1d743d49d8bb69f3bd79a
Jared Morgan <jmorgan@morgancreative.net>
2022-04-23 18:19:33 -0500

    Updates for monorepo

---
+++ packages/app/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>Bracket</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
