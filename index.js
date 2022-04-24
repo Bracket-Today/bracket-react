4ecd7e8d6d0e65749ab034ee4476acc7b6a8953b
Jared Morgan <jmorgan@morgancreative.net>
2022-04-23 22:08:47 -0500

    Finalize mobile setup
    - Have not tried android
    - entry-file is not working right in ios, so just copied
      packages/mobile/index.js to /index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from 'app/src/App';
+import {name as appName} from 'mobile/app.json';
+
+AppRegistry.registerComponent(appName, () => App);
