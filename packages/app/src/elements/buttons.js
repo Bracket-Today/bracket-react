1dad07e2ce7fe750fc16d7ca75d011f4b53d0a11
Jared Morgan <jmorgan@morgancreative.net>
2022-07-03 21:29:20 -0500

    8c0585 - Add button element

---
+++ packages/app/src/elements/buttons.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { View, Pressable } from 'react-native';
+import styled from 'styled-components/native';
+
+import { Link } from 'app/src/utils/routing';
+import { HeaderText } from 'app/src/styles';
+import colors from 'app/src/styles/colors';
+import fonts from 'app/src/styles/fonts';
+
+const CONTAINER_STYLES = `
+  background-color: ${colors.button};
+  width: 200px;
+  padding: 6px;
+  border-radius: 6px;
+  text-align: center;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
+
+const Container = styled(View)`
+  ${CONTAINER_STYLES}
+`;
+
+const PressableContainer = styled(Pressable)`
+  ${CONTAINER_STYLES}
+`;
+
+const ButtonText = styled(HeaderText)`
+  font-family: '${fonts.header}';
+  color: white;
+  font-weight: 800;
+  text-decoration-line: none;
+`;
+
+export const Button = ({ to, label, ...props }) => {
+  props.style ||= {}
+
+  if ('Cancel' === props.type) {
+    props.style.backgroundColor ||= '#505050';
+  }
+
+  const content = <ButtonText>{label}</ButtonText>;
+
+  if (to) {
+    return (
+      <Container {...props}>
+        <Link to={to} style={{textDecoration: 'none'}}>{content}</Link>
+      </Container>
+    );
+  } else {
+    return <PressableContainer {...props}>{content}</PressableContainer>;
+  }
+
+  return content;
+}
--- packages/app/src/styles/colors.js
+++ packages/app/src/styles/colors.js
@@ -1,5 +1,6 @@
 const colors = {
   screen: '#433552',
+  button: '#23AD23',
 };
 
 export default colors;
