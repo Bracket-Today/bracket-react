5c26494ba024f0fa0541a13ea48720f91fc401b1
Jared Morgan <jmorgan@morgancreative.net>
2022-10-25 20:53:33 -0500

    c6bc10 - Announcements - mobile link on next line

--- packages/app/src/components/Announcements/show.js
+++ packages/app/src/components/Announcements/show.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 import styled from 'styled-components/native';
+import MediaQuery from 'react-native-web-responsive';
 
 import { Link } from 'app/src/utils/routing';
 import { Text, Subtitle } from 'app/src/styles';
@@ -15,25 +16,49 @@ const SubjectContainer = styled(View)`
   flex-direction: row;
 `;
 
+const SubjectContainerSmall = styled(View)`
+  flex-direction: column;
+`;
+
 const Subject = styled(Subtitle)`
   color: ${colors.screen};
 `;
 
+const LinkSmall = styled(Link)`
+  margin-top: 8px;
+`;
+
 const Details = styled(Text)`
+  margin-top: 8px;
   font-size: 14px;
 `;
 
 const Announcement = ({ announcement }) => {
+  const hasLink = announcement.linkText && announcement.url;
+
   return (
     <Container>
-      <SubjectContainer>
-        <Subject>{announcement.subject}</Subject>
-        {announcement.linkText && announcement.url && (
-          <Link to={announcement.url}>
-            <Subject> → {announcement.linkText}</Subject>
-          </Link>
-        )}
-      </SubjectContainer>
+      <MediaQuery minWidth={800}>
+        <SubjectContainer>
+          <Subject>{announcement.subject}</Subject>
+          {hasLink && (
+            <Link to={announcement.url}>
+              <Subject> → {announcement.linkText}</Subject>
+            </Link>
+          )}
+        </SubjectContainer>
+      </MediaQuery>
+      <MediaQuery maxWidth={799}>
+        <SubjectContainerSmall>
+          <Subject>{announcement.subject}</Subject>
+          {hasLink && (
+            <LinkSmall to={announcement.url}>
+              <Subject> → {announcement.linkText}</Subject>
+            </LinkSmall>
+          )}
+        </SubjectContainerSmall>
+      </MediaQuery>
+
       {announcement.details && (
         <Details>{announcement.details}</Details>
       )}
